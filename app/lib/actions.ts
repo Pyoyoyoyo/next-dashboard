f1d42cd2fd4f92a11f2fe97afc093cec5f0c3f7e
Pyoyoyoyo <sodbileg.dorjbayar@gmail.com>
2024-10-29 12:00:14 +0800

    fixing errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,7 +16,7 @@ export async function authenticate(
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case 'CredentialsSignin':
+        case 'CredentialsSigning':
           return 'Invalid credentials.';
         default:
           return 'Something went wrong.';
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -6,14 +6,13 @@ import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
-  customers,
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return <form action={formAction}></form>;
 }
